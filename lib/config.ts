dabc2ade3e0c5cd6bcc92d2a280691fa2cb577f7
Nguyễn Văn Linh <linhnv1622@gmail.com>
2024-10-29 00:53:36 +0700

    update code

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,19 @@
-export const COURSES_BACKEND = 'http://localhost:3100/courses';
+export const COURSES_BACKEND =
+  process.env.REACT_APP_STAGE === 'development'
+    ? 'http://localhost:3000/courses'
+    : 'https://be-english-courses.kuma.id.vn/courses';
 
-export const CLASSROON_BACKEND = 'http://localhost:3100/classroom';
+export const CLASSROON_BACKEND =
+  process.env.REACT_APP_STAGE === 'development'
+    ? 'http://localhost:3100/classroom'
+    : 'https://be-english-courses.kuma.id.vn/classroom';
 
-export const STUDENTS_BACKEND = 'http://localhost:3200/students';
+export const STUDENTS_BACKEND =
+  process.env.REACT_APP_STAGE === 'development'
+    ? 'http://localhost:3200/students'
+    : 'https://be-english-person.id.vn/students';
 
-export const TEACHERS_BACKEND = 'http://localhost:3200/teachers';
+export const TEACHERS_BACKEND =
+  process.env.REACT_APP_STAGE === 'development'
+    ? 'http://localhost:3200/teachers'
+    : 'https://be-english-person.id.vn/teachers';
